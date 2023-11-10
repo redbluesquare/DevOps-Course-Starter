e992228a8cffb9336a91bcdffe6ca41c25635c63
DU71DE <darryl.usher@uk.bosch.com>
2023-11-10 15:56:46 +0000

    Updated app to add new items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, redirect, render_template, request
+from todo_app.data.session_items import add_item, get_items
 
 from todo_app.flask_config import Config
 
@@ -7,7 +7,12 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    items = get_items()
-    return render_template('index.html', items = items)
+    if request.method == 'GET':
+        items = get_items()
+        return render_template('index.html', items = items)
+    elif request.method == 'POST':
+        title = request.form.get('title')
+        add_item(title)
+        return redirect('/')
