bd53b5906085632218f95cc8fbf9f1d0a67cf153
DU71DE <darryl.usher@uk.bosch.com>
2024-01-05 07:23:48 +0000

    Created new branch to resolve issues with sync.

---
+++ todo_app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, render_template, request
+from todo_app.data.session_items import add_item, get_items
+
+from todo_app.flask_config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config())
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        items = get_items()
+        return render_template('index.html', items = items)
+    elif request.method == 'POST':
+        title = request.form.get('title')
+        add_item(title)
+        return redirect('/')
---
+++ todo_app/flask_config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config:
+    def __init__(self):
+        """Base configuration variables."""
+        self.SECRET_KEY = os.environ.get('SECRET_KEY')
+        if not self.SECRET_KEY:
+            raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
