e2e80e4096e2e569e0b0cc26d8f6e5ff5127ae70
DU71DE <darryl.usher@uk.bosch.com>
2024-02-22 13:40:01 +0000

    Implemented view-model

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, request
 from todo_app.data.trello_items import add_item, get_items, update_item
+from todo_app.data.view_model import ViewModel
 
 from todo_app.flask_config import Config
 
@@ -10,7 +11,8 @@ app.config.from_object(Config())
 @app.route('/')
 def index():
     items = get_items()
-    return render_template('index.html', items = items)
+    item_view_model = ViewModel(items)
+    return render_template('index.html', view_model = item_view_model)
 
 @app.route('/add-todo', methods=['POST'])
 def add_todo():
