0fbf0c88ace1a52e1d47f2c84693dd0774572e64
DU71DE <darryl.usher@uk.bosch.com>
2024-02-22 17:51:34 +0000

    Added create_app() function

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,24 +4,26 @@ from todo_app.data.view_model import ViewModel
 
 from todo_app.flask_config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
+    @app.route('/')
+    def index():
+        items = get_items()
+        item_view_model = ViewModel(items)
+        return render_template('index.html', view_model = item_view_model)
 
-@app.route('/')
-def index():
-    items = get_items()
-    item_view_model = ViewModel(items)
-    return render_template('index.html', view_model = item_view_model)
+    @app.route('/add-todo', methods=['POST'])
+    def add_todo():
+        title = request.form.get('title')
+        add_item(title)
+        return redirect('/')
 
-@app.route('/add-todo', methods=['POST'])
-def add_todo():
-    title = request.form.get('title')
-    add_item(title)
-    return redirect('/')
-
-@app.route('/close-todo', methods=['POST'])
-def close_todo():
-    id = request.form.get('id')
-    update_item(id, True)
-    return redirect('/')
+    @app.route('/close-todo', methods=['POST'])
+    def close_todo():
+        id = request.form.get('id')
+        update_item(id, True)
+        return redirect('/')
+    
+    return app
