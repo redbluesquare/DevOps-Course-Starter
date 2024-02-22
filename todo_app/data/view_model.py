e0110abfd574311aa68d45aed3c57a4b735a70d2
DU71DE <darryl.usher@uk.bosch.com>
2024-02-22 17:45:23 +0000

    updated done_items after implementing test and created open_items test

--- todo_app/data/view_model.py
+++ todo_app/data/view_model.py
@@ -3,5 +3,13 @@ class ViewModel:
         self._items = items
  
     @property
-    def items(self):
-        return self._items
+    def open_items(self):
+        return []
+    
+    @property
+    def done_items(self):
+        output = []
+        for item in self._items:
+            if item.status == 'Closed':
+                output.append(item)
+        return output
