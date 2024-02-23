a51923fdda2da35adff86d893743ab010653deb1
DU71DE <darryl.usher@uk.bosch.com>
2024-02-23 04:53:19 +0000

    Add test_app.py for integration testing

---
+++ todo_app/test_app.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv, find_dotenv
+import pytest
+from todo_app import app
+
+@pytest.fixture
+def client():
+    # Use our test integration config instead of the 'real' version
+    file_path = find_dotenv('.env.test')
+    load_dotenv(file_path, override=True)
+
+    # Create the new app.
+    test_app = app.create_app()
+
+    # Use the app to create a test_client that can be used in our tests.
+    with test_app.test_client() as client:
+        yield client
+
+def test_fixture(client):
+    assert False
